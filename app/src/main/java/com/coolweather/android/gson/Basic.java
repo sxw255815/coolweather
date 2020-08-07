537ccf014a28c451e12a76ebbc9ba75f6e9b6a7c
chenzs0826 <chenzs0826@wingtech.com>
2020-08-07 17:05:27 +0800

    加入显示天气信息功能

---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+
+
+    @SerializedName("city")
+    public String cityName;
+
+    public Update update;
+
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
